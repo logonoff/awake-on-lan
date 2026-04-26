09a7804099f93b01a5306bf56c11676f34a44904
logonoff <git@logonoff.co>
2026-04-26 15:28:49 -0400

    fix: add missing dep for macos

--- Formula/awakeonlan.rb
+++ Formula/awakeonlan.rb
@@ -3,7 +3,7 @@ class Awakeonlan < Formula
   homepage "https://github.com/logonoff/awake-on-lan"
   version "0.5.2"
   url "https://github.com/logonoff/awake-on-lan/releases/download/#{version}/awakeonlan-#{version}.tar.xz"
-  sha256 "cf9b0a2c26e19162619d32342887abe67fa48e762419fcdae5f55a62c22e051f"
+  sha256 "ff7bbc4fcbfe0d0db180ae247f4ca1d8c5e69ab143f3d9dd47e0b56b477bb8c2"
   license "GPL-3.0-or-later"
 
   depends_on "desktop-file-utils" => :build
@@ -16,6 +16,7 @@ class Awakeonlan < Formula
   depends_on "glib"
   depends_on "gtk4"
   depends_on "libadwaita"
+  depends_on "pygobject3"
   depends_on "python@3.13"
 
   on_macos do
