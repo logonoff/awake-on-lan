0347b8120f2dec763b7ef8e93b057d75f2d30b1d
logonoff <git@logonoff.co>
2026-04-25 21:33:52 -0400

    feat: add release workflow and homebrew formula
    Adds GitHub Actions workflow to automate releases with source tarballs
    and Homebrew formula updates. When a version tag is pushed, the workflow
    creates a GitHub release, updates the formula, and syncs to homebrew-bucket.

---
+++ Formula/awakeonlan.rb
@@ -0,0 +1,36 @@
+class Awakeonlan < Formula
+  desc "Simple libadwaita-based Wake on LAN application for waking computers remotely"
+  homepage "https://github.com/logonoff/awake-on-lan"
+  version "0.0.0"
+  url "https://github.com/logonoff/awake-on-lan/releases/download/#{version}/awakeonlan-#{version}.tar.xz"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  license "GPL-3.0-or-later"
+
+  depends_on "desktop-file-utils" => :build
+  depends_on "gettext" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
+
+  depends_on "adwaita-icon-theme"
+  depends_on "glib"
+  depends_on "gtk4"
+  depends_on "libadwaita"
+  depends_on "python@3.13"
+
+  def install
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+    rm share/"glib-2.0/schemas/gschemas.compiled"
+  end
+
+  def post_install
+    system Formula["glib"].opt_bin/"glib-compile-schemas", HOMEBREW_PREFIX/"share/glib-2.0/schemas"
+    system Formula["gtk4"].opt_bin/"gtk4-update-icon-cache", "-f", "-t", HOMEBREW_PREFIX/"share/icons/hicolor"
+  end
+
+  test do
+    assert_predicate bin/"awakeonlan", :executable?
+  end
+end
